f48ff5323c2fe52bab9d9d9fd106976ac1257621
PavelPindarev <pavel09.1970@abv.bg>
2023-09-14 02:21:48 +0300

    Make a Queue to work faster by adding a tail and reducing algorithm complexity for offer(), from O(n) to O(1) complexity.

--- LinearDataStructure-Exercise/src/main/java/implementations/Queue.java
+++ LinearDataStructure-Exercise/src/main/java/implementations/Queue.java
@@ -6,6 +6,7 @@ import java.util.Iterator;
 
 public class Queue<E> implements AbstractQueue<E> {
     private Node<E> head;
+    private Node<E> tail;
     private int size;
 
     private static class Node<E> {
@@ -18,19 +19,19 @@ public class Queue<E> implements AbstractQueue<E> {
     }
 
     public Queue() {
+        this.head = null;
+        this.tail = null;
+        this.size = 0;
     }
 
     @Override
     public void offer(E element) {
         Node<E> newNode = new Node<>(element);
         if (this.head == null) {
-            this.head = newNode;
+            this.head = this.tail = newNode;
         } else {
-            Node<E> current = this.head;
-            while (current.next != null) {
-                current = current.next;
-            }
-            current.next = newNode;
+            this.tail.next = newNode;
+            this.tail = newNode;
         }
         this.size++;
     }
@@ -40,7 +41,7 @@ public class Queue<E> implements AbstractQueue<E> {
         ensureNonEmpty();
         E element = this.head.element;
         if (this.size == 1) {
-            this.head = null;
+            this.head = this.tail = null;
         } else {
             Node<E> next = this.head.next;
             this.head.next = null;
