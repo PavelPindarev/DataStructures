6e33229d8b7e4b7a29b15561ff427b39f9f87840
PavelPindarev <pavel09.1970@abv.bg>
2023-09-15 19:19:45 +0300

    Implement BalancedParentheses and the method solve () – which performs analysis of the parentheses filed and returns true or false whether the parentheses are balanced or no.

--- LinearDataStructure-Exercise/src/main/java/implementations/BalancedParentheses.java
+++ LinearDataStructure-Exercise/src/main/java/implementations/BalancedParentheses.java
@@ -5,12 +5,45 @@ import interfaces.Solvable;
 public class BalancedParentheses implements Solvable {
     private String parentheses;
 
+    private int head;
+    private int tail;
+
     public BalancedParentheses(String parentheses) {
         this.parentheses = parentheses;
+        this.head = 0;
+        this.tail = parentheses.length() - 1;
     }
 
     @Override
     public Boolean solve() {
-        return null;
+        if (isSequenceOdd()) {
+            return false;
+        }
+        int timesOfIteration = this.parentheses.length() / 2;
+        for (int i = 0; i < timesOfIteration; i++) {
+            char firstSymbol = this.parentheses.charAt(this.head++);
+            char lastSymbol = this.parentheses.charAt(this.tail--);
+
+            if (!checkPalindrome(firstSymbol, lastSymbol)) {
+                return false;
+            }
+
+        }
+        return true;
+    }
+
+    private boolean checkPalindrome(char firstSymbol, char lastSymbol) {
+        switch (firstSymbol) {
+            case '(':
+                return firstSymbol + 1 == lastSymbol;
+            case '{':
+            case '[':
+                return firstSymbol + 2 == lastSymbol;
+        }
+        return false;
+    }
+
+    private boolean isSequenceOdd() {
+        return this.parentheses.length() / 2 == 1;
     }
 }
