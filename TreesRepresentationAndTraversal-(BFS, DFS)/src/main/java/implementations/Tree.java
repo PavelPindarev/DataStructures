7ffa21803b2a16aabf2fdb6ea235ad6454075fa9
PavelPindarev <pavel09.1970@abv.bg>
2023-09-16 17:32:58 +0300

    Initial commit of Trees Representation and Traversal (BFS, DFS).

---
+++ TreesRepresentationAndTraversal-(BFS, DFS)/src/main/java/implementations/Tree.java
@@ -0,0 +1,36 @@
+package implementations;
+
+import interfaces.AbstractTree;
+
+import java.util.List;
+
+public class Tree<E> implements AbstractTree<E> {
+
+    @Override
+    public List<E> orderBfs() {
+        return null;
+    }
+
+    @Override
+    public List<E> orderDfs() {
+        return null;
+    }
+
+    @Override
+    public void addChild(E parentKey, Tree<E> child) {
+
+    }
+	
+	@Override
+    public void removeNode(E nodeKey) {
+
+    }
+
+    @Override
+    public void swap(E firstKey, E secondKey) {
+
+    }
+}
+
+
+
