0368f17d3ed8028ed2a50843b9c2668517adca3a
PavelPindarev <pavel09.1970@abv.bg>
2023-09-24 20:44:59 +0300

    Implementation of BinaryTree.

--- Heaps, BST/src/main/java/implementations/BinaryTree.java
+++ Heaps, BST/src/main/java/implementations/BinaryTree.java
@@ -7,49 +7,118 @@ import java.util.List;
 import java.util.function.Consumer;
 
 public class BinaryTree<E> implements AbstractBinaryTree<E> {
+    private E key;
+
+    private BinaryTree<E> left;
+
+    private BinaryTree<E> right;
+
+    public BinaryTree(E key, BinaryTree<E> left, BinaryTree<E> right) {
+        this.setKey(key);
+        this.left = left;
+        this.right = right;
+    }
 
     @Override
     public E getKey() {
-        return null;
+        return this.key;
     }
 
     @Override
     public AbstractBinaryTree<E> getLeft() {
-        return null;
+        return this.left;
     }
 
     @Override
     public AbstractBinaryTree<E> getRight() {
-        return null;
+        return this.right;
     }
 
     @Override
     public void setKey(E key) {
-
+        this.key = key;
     }
 
     @Override
     public String asIndentedPreOrder(int indent) {
-        return null;
+        StringBuilder result = new StringBuilder();
+        result.append(this.getPadding(indent)).append(this.getKey());
+
+        if (this.getLeft() != null) {
+            result.append(System.lineSeparator())
+                    .append(this.getLeft().asIndentedPreOrder(indent + 2));
+        }
+
+        if (this.getRight() != null) {
+            result.append(System.lineSeparator())
+                    .append(this.getRight().asIndentedPreOrder(indent + 2));
+        }
+
+        return result.toString();
     }
+
+    private String getPadding(int indent) {
+        StringBuilder builder = new StringBuilder();
+        for (int i = 0; i < indent; i++) {
+            builder.append(" ");
+        }
+        return builder.toString();
+    }
+
 
     @Override
     public List<AbstractBinaryTree<E>> preOrder() {
-        return null;
+        List<AbstractBinaryTree<E>> trees = new ArrayList<>();
+
+        trees.add(this);
+        if (this.getLeft() != null) {
+            trees.addAll(this.getLeft().preOrder());
+        }
+        if (this.getRight() != null) {
+            trees.addAll(this.getRight().preOrder());
+        }
+
+        return trees;
     }
 
     @Override
     public List<AbstractBinaryTree<E>> inOrder() {
-        return null;
+        List<AbstractBinaryTree<E>> trees = new ArrayList<>();
+
+        if (this.getLeft() != null) {
+            trees.addAll(this.getLeft().inOrder());
+        }
+        trees.add(this);
+        if (this.getRight() != null) {
+            trees.addAll(this.getRight().inOrder());
+        }
+
+        return trees;
     }
 
     @Override
     public List<AbstractBinaryTree<E>> postOrder() {
-        return null;
+        List<AbstractBinaryTree<E>> trees = new ArrayList<>();
+
+        if (this.getLeft() != null) {
+            trees.addAll(this.getLeft().postOrder());
+        }
+        if (this.getRight() != null) {
+            trees.addAll(this.getRight().postOrder());
+        }
+        trees.add(this);
+
+        return trees;
     }
 
     @Override
     public void forEachInOrder(Consumer<E> consumer) {
-
+        if (this.getLeft() != null) {
+            this.getLeft().forEachInOrder(consumer);
+        }
+        consumer.accept(this.key);
+        if (this.getRight() != null) {
+            this.getRight().forEachInOrder(consumer);
+        }
     }
 }
