54696d1653e5930140efb4b6d66568e3a0c3aaa0
PavelPindarev <pavel09.1970@abv.bg>
2023-09-26 18:47:03 +0300

    Implementation of CookiesProblems.

--- Heaps, BST-Exercise/src/main/java/solutions/CookiesProblem.java
+++ Heaps, BST-Exercise/src/main/java/solutions/CookiesProblem.java
@@ -1,7 +1,28 @@
 package solutions;
 
+import java.util.PriorityQueue;
+import java.util.Queue;
+
 public class CookiesProblem {
     public Integer solve(int k, int[] cookies) {
-        return null;
+        Queue<Integer> queue = new PriorityQueue<>();
+
+        for (int cookie : cookies) {
+            queue.offer(cookie);
+        }
+        int currentMinSweetness = queue.peek();
+        int steps = 0;
+        while (currentMinSweetness < k && queue.size() > 1) {
+            int leastSweet = queue.poll();
+            int secondLeastSweet = queue.poll();
+
+            int combinedSweetness = leastSweet + 2 * secondLeastSweet;
+
+            queue.offer(combinedSweetness);
+
+            currentMinSweetness = queue.peek();
+            steps++;
+        }
+        return currentMinSweetness > k ? steps : -1;
     }
 }
