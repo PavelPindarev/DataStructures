4859fd751de9c919988e538e320dcdaf021fe6b9
PavelPindarev <pavel09.1970@abv.bg>
2023-09-26 17:33:52 +0300

    Implementation of BinaryTree find LowestCommonAncestor.

--- Heaps, BST-Exercise/src/main/java/solutions/BinaryTree.java
+++ Heaps, BST-Exercise/src/main/java/solutions/BinaryTree.java
@@ -3,12 +3,30 @@ package solutions;
 import java.util.List;
 
 public class BinaryTree {
+    private int key;
+    private BinaryTree left;
+    private BinaryTree right;
 
     public BinaryTree(int key, BinaryTree first, BinaryTree second) {
-
+        this.key = key;
+        this.left = first;
+        this.right = second;
     }
 
     public Integer findLowestCommonAncestor(int first, int second) {
+        int lower = Math.min(first, second);
+        int upper = Math.max(first, second);
+
+        BinaryTree current = this;
+        while (current != null) {
+            if (lower <= current.key && upper >= current.key) {
+                return current.key;
+            } else if (lower > current.key) {
+                current = current.right;
+            } else {
+                current = current.left;
+            }
+        }
         return null;
     }
 
